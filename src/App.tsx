5e2004bbbf1e2ad62ec9d80e3fb89e15ca57808c
Konstantinos Liberopoulos <libekonst@gmail.com>
2019-05-17 18:25:52 +0300

    Implement basic layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './normalize.css';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return <View />;
+};
 
 export default App;
+
+const View: React.FC = () => {
+  return (
+    <article className="panel">
+      <section className="panel__left">
+        <ul>
+          {Array(20)
+            .fill(0)
+            .map((el, i) => (
+              <li key={i} className="list-item">
+                hello {i}
+              </li>
+            ))}
+        </ul>
+      </section>
+      <section className="panel__right">hello from right</section>
+    </article>
+  );
+};
