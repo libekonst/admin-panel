31952eddaf2102b3a9c632ffc00f3b4a796520f0
Konstantinos Liberopoulos <libekonst@gmail.com>
2019-05-20 16:16:43 +0300

    Clean up form state methods. Add comments.

--- src/components/user-form/UserForm.tsx
+++ src/components/user-form/UserForm.tsx
@@ -14,13 +14,17 @@ interface IState {
   hasChanged: boolean;
 }
 
+/** A container component for the form's internal state. The user and the save service are provided by the parent.  */
 export class UserForm extends React.Component<IProps, IState> {
   state: IState = {
     inputs: getUserInputs(this.props.user),
     hasChanged: false,
   };
 
-  // If this is the first time changing the input, call an additional setState to enable the buttons.
+  /**
+   * If this is the first time changing the input, call an additional setState to enable the buttons.
+   * Could use `e.target.name` instead of first param, but might not be safe because the `name` attr is optional.
+   */
   handleChange = (name: keyof IUSerInputs) => (e: any) => {
     e.persist();
     if (!this.state.hasChanged) this.setState({ hasChanged: true });
@@ -30,12 +34,7 @@ export class UserForm extends React.Component<IProps, IState> {
     }));
   };
 
-  // Resets the buttons and the inputs to their initial values.
-  handleCancel = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    this.setState({ hasChanged: false, inputs: getUserInputs(this.props.user) });
-  };
-
+  /** Use html form validation. If input is valid, call the save service received from props and reset the buttons. */
   handleSave = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!e.currentTarget.checkValidity()) return;
@@ -44,9 +43,19 @@ export class UserForm extends React.Component<IProps, IState> {
     this.setState({ hasChanged: false });
   };
 
+  /** Resets the buttons and the inputs to their initial values. */
+  resetState = () =>
+    this.setState({ hasChanged: false, inputs: getUserInputs(this.props.user) });
+
+  /** When the reset event fires, reset the form's state. */
+  handleCancel = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    this.resetState();
+  };
+
+  /** If the selected user has changed, reset the form's state. */
   componentDidUpdate(prevProps: IProps) {
-    if (this.props.user !== prevProps.user)
-      this.setState({ inputs: getUserInputs(this.props.user) });
+    if (this.props.user !== prevProps.user) this.resetState();
   }
 
   render() {
--- src/components/user-form/getUserInputs.ts
+++ src/components/user-form/getUserInputs.ts
@@ -1,6 +1,11 @@
 import { IUser } from '../../interfaces/IUser';
 import { IUSerInputs } from '../../interfaces/IUserInputs';
 
+/** 
+ * Extracts an `IUser`'s editable properties into an object
+ * or create's a mock object with the same properties if no user is provided.
+ * @returns An `IUserInputs` object used by the user form as the initial inputs state.
+ */
 export function getUserInputs(user?: IUser): IUSerInputs {
   if (!user)
     return {
