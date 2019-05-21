ca830b224c54cabe6c89e46bdf07b53666598a14
Konstantinos Liberopoulos <libekonst@gmail.com>
2019-05-22 00:42:09 +0300

    Updates

---
+++ precache-manifest.ca1d88d00e996b02dd0097e8e6bcd3d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df0eb81031845ead5a9e9df35025eba",
+    "url": "/admin-panel/index.html"
+  },
+  {
+    "revision": "516b56ff56969ae14983",
+    "url": "/admin-panel/static/css/main.2ebf6a36.chunk.css"
+  },
+  {
+    "revision": "2515d0c2c16a5ccc04d5",
+    "url": "/admin-panel/static/js/2.d92c5276.chunk.js"
+  },
+  {
+    "revision": "516b56ff56969ae14983",
+    "url": "/admin-panel/static/js/main.00d8f3f7.chunk.js"
+  },
+  {
+    "revision": "ec38d16f4a227f006ad0",
+    "url": "/admin-panel/static/js/runtime~main.cb34ca52.js"
+  }
+]);
