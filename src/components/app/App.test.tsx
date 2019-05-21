9e4e8fd290687becf88a3d21c3d13321f7e6606f
Konstantinos Liberopoulos <libekonst@gmail.com>
2019-05-21 19:56:29 +0300

    Clean up .state method calls.

--- src/components/app/App.test.tsx
+++ src/components/app/App.test.tsx
@@ -41,64 +41,64 @@ describe('The <App/> component', () => {
   });
 
   it('initially has no user selected', () => {
-    const component = shallow(<App users={users} />);
-    expect(component.state('selected')).toBeUndefined();
+    const component = shallow<App>(<App users={users} />);
+    expect(component.state().selectedID).toBeUndefined();
   });
 
   it('should provide a method to select a user and re-render the View with a selected user', () => {
     const { id } = users[0];
-    const component = shallow(<App users={users} />);
+    const component = shallow<App>(<App users={users} />);
     const instance = component.instance() as App;
 
     // Assert there is no initial selected user.
     expect(component).toMatchSnapshot();
-    expect(component.state('selected')).toBeUndefined();
+    expect(component.state().selectedID).toBeUndefined();
 
     // Call the handleSelectUSer method with an id.
     instance.handleSelectUser(id)();
 
     // Asssert that a user is now selected.
-    expect(component.state('selected')).toBe(id);
+    expect(component.state().selectedID).toBe(id);
     expect(component).toMatchSnapshot();
   });
 
   it("has a handleSave method that updates a user's data", () => {
     const { id } = users[0];
-    const component = shallow(<App users={users} />);
+    const component = shallow<App>(<App users={users} />);
     const instance = component.instance() as App;
     const updated = users.map(u => (u.id === id ? { ...u, ...inputs } : u));
 
     // Select a user.
-    instance.setState({ selected: id });
-    expect(component.state('selected')).toBe(id);
+    instance.setState({ selectedID: id });
+    expect(component.state().selectedID).toBe(id);
 
     // Call the handleSave method and update the user's properties.
     // Assert the users state is updated.
     instance.handleSave(inputs);
-    expect(component.state('users')).toEqual(updated);
+    expect(component.state().users).toEqual(updated);
   });
 
   it("the handleSave method should do nothing if there is no selected user or it can't find a user", () => {
     const id = 'user-id';
-    const component = shallow(<App users={users} />);
+    const component = shallow<App>(<App users={users} />);
     const instance = component.instance() as App;
 
     // Test initial state.
-    expect(component.state('selected')).toBeUndefined();
-    expect(component.state('users')).toEqual(users);
+    expect(component.state().selectedID).toBeUndefined();
+    expect(component.state().users).toEqual(users);
 
     // Call the handleSave method.
     // Assert the initial state hasn't changed as long as the selected id is undefined.
     instance.handleSave(inputs);
-    expect(component.state('users')).toEqual(users);
+    expect(component.state().users).toEqual(users);
 
     // Set selected id.
-    instance.setState({ selected: id });
-    expect(component.state('selected')).toBe(id);
+    instance.setState({ selectedID: id });
+    expect(component.state().selectedID).toBe(id);
 
     // Call the handleSave method.
     // Assert the initial users state hasn't change since the user isn't be found.
     instance.handleSave(inputs);
-    expect(component.state('users')).toEqual(users);
+    expect(component.state().users).toEqual(users);
   });
 });
